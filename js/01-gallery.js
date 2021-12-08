490caa28da6f4d045a57169f3d47186ef84842e5
RuslanKabachok <ruslan.kabachok@gmail.com>
2021-12-08 20:46:38 +0200

    задача №1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const markUp = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markUp);
